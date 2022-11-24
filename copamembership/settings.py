9121de247fcbf680669d8f90d9a8eacccf6f982e
shaphan <chaphannzabonimana@gmail.com>
2022-11-24 14:30:04 +0200

    update allowed hosts

--- copamembership/settings.py
+++ copamembership/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-8t#%(m4w$i*r!jkp*ei02&=sjk1%*&rkac*(+j@wbn2%e#6+py
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
