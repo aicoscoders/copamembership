bd9580aa9eeed79af63093c3bd1b97266eb31c2e
uwase610 <uwasegaellaolga@gmail.com>
2022-11-25 09:55:23 +0200

    updating user details

--- users/schema.py
+++ users/schema.py
@@ -10,8 +10,7 @@ class UserType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
-
+    users = graphene.List(UserType) 
     def resolve_users(root, info):
         return User.objects.all()
 
@@ -35,6 +34,27 @@ class CreateUser(graphene.Mutation):
         user1.save()
         return CreateUser(user=user1)
 
+class UpdateUser(graphene.Mutation):
+    class Arguments:
+        name=graphene.String(required=True)
+        email = graphene.String(required=True)
+        phone = graphene.String(required=True)
+        id = graphene.ID()
+    
+    user = graphene.Field(UserType)
+
+    @classmethod
+    def mutate(cls, root, info, name, email, phone, id):
+        user1=User.objects.get(pk=id)
+        user1=User(
+            name=name,
+            email=email,
+            phone=phone
+        )
+        user1.save()
+        return UpdateUser(user=user1)
+
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    update_user = UpdateUser.Field()
